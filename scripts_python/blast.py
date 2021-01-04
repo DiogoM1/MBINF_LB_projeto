cec59d3f36d46e4d0ae244b43ff39bc324bb29e4
AngelinaEiras <73119057+AngelinaEiras@users.noreply.github.com>
2021-01-04 19:25:32 +0000

    notebook genebank começado, blast começado mais a sério

--- scripts_python/blast.py
+++ scripts_python/blast.py
@@ -1,4 +1,4 @@
-
+# aqui caralho
 '''
 Análise de homologias por BLAST
 As ferramentas de procura de homologias serão de especial relevo, nomeadamente para a procura de genes homólogos, bem como para a caracterização funcional dos genes selecionados.
@@ -10,55 +10,16 @@ As ferramentas de procura de homologias serão de especial relevo, nomeadamente 
 Deverá implementar scripts Python/ BioPython para automatizar estas tarefas.
 '''
 
-############################### BLAST
-
-from Bio import SeqIO
 from Bio.Blast import NCBIWWW
-import os
-
-ficheiro=r'C:\Users\angie\PycharmProjects\MBINF_LB_projeto\data\resultados_GeneBank\BST2_1519313265.fasta'
-fasta_string = open(ficheiro).read()
-print(fasta_string)
-result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)
-txt=result_handle.read()
-print(txt)
-
-#parser e depois por ordem definição do melhor e score
-
-#record = SeqIO.read("m_cold.fasta", format="fasta")
-#result_handle = NCBIWWW.qblast("blastn", "nt", record.format("fasta"))
-#with open("my_blast.xml", "w") as out_handle:
-#    out_handle.write(result_handle.read())
-#result_handle.close()
-#result_handle = open("my_blast.xml")
 
 
-def find_homolg(ficheiro):
-    '''
-    Usar ficheiros existentes na nossa posse, encontrar homologos
-    :param ficheiro:
-    :return: homologos lindos
-    '''
-    fasta_string = open(str(ficheiro)).read()
-    result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)
-    return result_handle
+def homolg_id(id, location = None):
+    result_handle = NCBIWWW.qblast("blastn", "nt", id)
+    for i in range(0,10):
+        aln = result_handle.alignments[i]
+        print(aln.title)
 
 
-
-
-def function_from_homolg(gene):
-    pass
-
-
-def main():
-    os.chdir('..')
-    os.chdir('data/resultados_GeneBank')
-    with open(r'resultados_GeneBank', 'rt') as fich: #Because "r" for read, and "t" for text are the default values, you do not need to specify them.
-        for f in fich:
-            # fich.read(f, 'rt')
-            find_homolg(f)
-
-    # ou ver https://stackoverflow.com/questions/18262293/how-to-open-every-file-in-a-folder
-
-
-# main()
+#TODO: fazer blast e guardar ficheiro
+#TODO: filtrar o ID de todos os homologos
+#TODO: Correr
