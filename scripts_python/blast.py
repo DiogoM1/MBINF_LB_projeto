847e3c404c8e9951f2de458cc466dbdea6612695
dm <diogodpmacedo@gmail.com>
2021-01-13 23:54:23 +0000

    init new site

--- scripts_python/blast.py
+++ scripts_python/blast.py
@@ -1,71 +1,63 @@
-'''
-Análise de homologias por BLAST
-As ferramentas de procura de homologias serão de especial relevo, nomeadamente para a procura de genes homólogos, bem como para a caracterização funcional dos genes selecionados.
-
-    No primeiro caso, deverá configurar adequadamente as suas pesquisas ao nível da base de dados e desenvolver código para automatizar a decisão de existência de homologias significativas.
-
-    No segundo caso, poderá analisar a lista de sequências homólogas e identificar padrões consistentes ao nível da função desempenhada por estas.
-
-Deverá implementar scripts Python/ BioPython para automatizar estas tarefas.
-'''
-
-############################### BLAST
-
-
 from Bio.Blast import NCBIWWW, NCBIXML
 import os
 
+class BLASTer:
+    def __init__(self, assession, fasta):
+        self.assession = assession
 
-def blaster(ID):
-    result_handle = NCBIWWW.qblast("blastn", "nt", str(ID))
-    txt = result_handle.read()
-    print(txt)
+    def blast(ID):
+        result_handle = NCBIWWW.qblast("blastn", "nt", str(ID))
+        txt = result_handle.read()
+        print(txt)
 
-
-
-#parser e depois por ordem definição do melhor e score
-
-#record = SeqIO.read("m_cold.fasta", format="fasta")
-#result_handle = NCBIWWW.qblast("blastn", "nt", record.format("fasta"))
-#with open("my_blast.xml", "w") as out_handle:
-#    out_handle.write(result_handle.read())
-#result_handle.close()
-#result_handle = open("my_blast.xml")
+    def blast_from_file(self):
+        return
 
 
-def find_homolg_files(ficheiro):
-    '''
-    Usar ficheiros existentes na nossa posse, encontrar homologos
-    :param ficheiro:
-    :return: homologos lindos
-    '''
-    fasta_string = open(str(ficheiro)).read()
-    result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)
-    return result_handle
+
+    """#parser e depois por ordem definição do melhor e score
+
+    #record = SeqIO.read("m_cold.fasta", format="fasta")
+    #result_handle = NCBIWWW.qblast("blastn", "nt", record.format("fasta"))
+    #with open("my_blast.xml", "w") as out_handle:
+    #    out_handle.write(result_handle.read())
+    #result_handle.close()
+    #result_handle = open("my_blast.xml")"""
 
 
-def function_from_homolg(gene):
-    pass
+    """def find_homolg_files(ficheiro):
+        '''
+        Usar ficheiros existentes na nossa posse, encontrar homologos
+        :param ficheiro:
+        :return: homologos lindos
+        '''
+        fasta_string = open(str(ficheiro)).read()
+        result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)
+        return result_handle"""
 
 
-def main():
-    os.chdir('..')
-    os.chdir('data/GeneBank')
-    with open(r'GeneBank', 'rt') as fich: #Because "r" for read, and "t" for text are the default values, you do not need to specify them.
-        for f in fich:
-            # fich.read(f, 'rt')
-            find_homolg(f)
-
-    # ou ver https://stackoverflow.com/questions/18262293/how-to-open-every-file-in-a-folder
-
-def homolg_id(id, location = None):
-    result_handle = NCBIWWW.qblast("blastn", "nt", id)
-    for i in range(0,10):
-        aln = result_handle.alignments[i]
-        print(aln.title)
+    """def function_from_homolg(gene):
+        pass"""
 
 
-#TODO: fazer blast e guardar ficheiro
-#TODO: filtrar o ID de todos os homologos
-#TODO: Correr
-# TODO: scores dos homologos e print por ordem?
+    """def main():
+        os.chdir('..')
+        os.chdir('data/GeneBank')
+        with open(r'GeneBank', 'rt') as fich: #Because "r" for read, and "t" for text are the default values, you do not need to specify them.
+            for f in fich:
+                # fich.read(f, 'rt')
+                find_homolg(f)"""
+
+        # ou ver https://stackoverflow.com/questions/18262293/how-to-open-every-file-in-a-folder
+
+   """ def homolg_id(id, location = None):
+        result_handle = NCBIWWW.qblast("blastn", "nt", id)
+        for i in range(0,10):
+            aln = result_handle.alignments[i]
+            print(aln.title)"""
+
+
+    #TODO: fazer blast e guardar ficheiro
+    #TODO: filtrar o ID de todos os homologos
+    #TODO: Correr
+    # TODO: scores dos homologos e print por ordem?
