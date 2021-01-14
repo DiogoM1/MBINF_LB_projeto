a3044ee7cd8a193e0cba734cf00d3e43a72087a4
dm <diogodpmacedo@gmail.com>
2021-01-14 12:57:03 +0000

    presentation ready

---
+++ lb/credits.md
@@ -0,0 +1,15 @@
+# Créditos
+
+Este projeto foi realizado por:
+- Angelina Eiras PG42861
+- Diogo Macedo PG42865
+- Pedro Gonçalves PG42879
+
+## Tarefas
+### Revisão bibliográfica:
+- Angelina Eiras
+- Pedro Gonçalves
+
+### Pesquisa automatizada do Pubmed:
+- Diogo Macedo
+- Pedro Gonçalves
--- scripts_python/blast.py
+++ scripts_python/blast.py
@@ -1,61 +1,29 @@
 from Bio.Blast import NCBIWWW, NCBIXML
 import os
 
-class BLASTer:
-    def __init__(self, assession, fasta):
-        self.assession = assession
 
-    def blast(ID):
-        result_handle = NCBIWWW.qblast("blastn", "nt", str(ID))
-        txt = result_handle.read()
-        print(txt)
+class BLASTer:
+    def __init__(self, accession="", fasta="", type="blastn", db="nt", e_thresh=0.05):
+        self.accession = str(accession)
+        self.fasta = fasta
+        self.type = type
+        self.db = db
+        self.e_thresh = e_thresh
+
+    def blast_from_accession(self):
+        result_handle = NCBIWWW.qblast(self.type, self.db, self.accession)
+        return result_handle
 
     def blast_from_file(self):
-        return
+        result_handle = NCBIWWW.qblast(self.type, database=self.db, sequence=self.fasta.format("fasta"))
+        return result_handle
 
-
-
-    """#parser e depois por ordem definição do melhor e score
-
-    #record = SeqIO.read("m_cold.fasta", format="fasta")
-    #result_handle = NCBIWWW.qblast("blastn", "nt", record.format("fasta"))
-    #with open("my_blast.xml", "w") as out_handle:
-    #    out_handle.write(result_handle.read())
-    #result_handle.close()
-    #result_handle = open("my_blast.xml")"""
-
-
-    """def find_homolg_files(ficheiro):
-        '''
-        Usar ficheiros existentes na nossa posse, encontrar homologos
-        :param ficheiro:
-        :return: homologos lindos
-        '''
-        fasta_string = open(str(ficheiro)).read()
-        result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)
-        return result_handle"""
-
-
-    """def function_from_homolg(gene):
-        pass"""
-
-
-    """def main():
-        os.chdir('..')
-        os.chdir('data/GeneBank')
-        with open(r'GeneBank', 'rt') as fich: #Because "r" for read, and "t" for text are the default values, you do not need to specify them.
-            for f in fich:
-                # fich.read(f, 'rt')
-                find_homolg(f)"""
-
-        # ou ver https://stackoverflow.com/questions/18262293/how-to-open-every-file-in-a-folder
-
-   """ def homolg_id(id, location = None):
-        result_handle = NCBIWWW.qblast("blastn", "nt", id)
-        for i in range(0,10):
-            aln = result_handle.alignments[i]
-            print(aln.title)"""
-
+    def blast_from_to_files(self):
+        result_handle = self.blast_from_file()
+        save_file = open("aula9_blast.xml", "w")
+        save_file.write(result_handle.read())
+        save_file.close()
+        result_handle.close()
 
     #TODO: fazer blast e guardar ficheiro
     #TODO: filtrar o ID de todos os homologos
