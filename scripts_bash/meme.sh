33e4bf14681a5da819794879c0b1fe9314bc8828
dm <diogodpmacedo@gmail.com>
2021-01-28 13:57:06 +0000

    finish MEMO pipeline, clustalo done

--- scripts_bash/meme.sh
+++ scripts_bash/meme.sh
@@ -4,10 +4,10 @@ BLAST_RES="$1"
 GENE="$2"
 WORKDIR="/opt/project/data"
 
-meme "${BLAST_RES}" -protein -mod zops -oc "${WORKDIR}/motif/${GENE}/meme" -nmotifs 5 -minw 6 -maxw 50 > "${WORKDIR}/motif/${GENE}/meme/meme_cli_report.txt"
+meme "${WORKDIR}/${BLAST_RES}" -protein -mod zoops -oc "${WORKDIR}/motif/${GENE}/meme" -nmotifs 5 -minw 6 -maxw 50
 
 # fasta-get-markov "${BLAST_RES}" "${WORKDIR}/motif/${GENE}/markov"
 
-fimo --bfile --motif-- -oc"${WORKDIR}/motif/${GENE}/fimo" "${WORKDIR}/motif/${GENE}/meme/meme.txt" "${BLAST_RES}"
+fimo --bfile --motif-- -oc "${WORKDIR}/motif/${GENE}/fimo" "${WORKDIR}/motif/${GENE}/meme/meme.txt" "${WORKDIR}/${BLAST_RES}"
 
-mast -bfile --motif-- -remcorr -oc"${WORKDIR}/motif/${GENE}/mast" "${WORKDIR}/motif/${GENE}/meme/meme.txt" "${BLAST_RES}"
+mast -bfile --motif-- -remcorr -oc "${WORKDIR}/motif/${GENE}/mast" "${WORKDIR}/motif/${GENE}/meme/meme.txt" "${WORKDIR}/${BLAST_RES}"
