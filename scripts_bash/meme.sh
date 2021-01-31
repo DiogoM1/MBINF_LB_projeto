5f5b3ab6df5d989043becdfc10653b316b9059ce
dm <diogodpmacedo@gmail.com>
2021-01-31 22:28:14 +0000

    clean up

--- scripts_bash/meme.sh
+++ scripts_bash/meme.sh
@@ -6,10 +6,10 @@ REF="$3"
 GENE="$4"
 WORKDIR="/opt/project/data"
 
-# meme "${WORKDIR}/${BLAST_RES_uni}" -protein -mod zoops -oc "${WORKDIR}/motif/${GENE}/meme" -nmotifs 5 -minw 6 -maxw 50
+meme "${WORKDIR}/${BLAST_RES_uni}" -protein -mod zoops -oc "${WORKDIR}/motif/${GENE}/meme" -nmotifs 5 -minw 6 -maxw 50
 
 # fasta-get-markov "${BLAST_RES}" "${WORKDIR}/motif/${GENE}/markov"
 
-# fimo --bfile --motif-- -oc "${WORKDIR}/motif/${GENE}/fimo" "${WORKDIR}/motif/${GENE}/meme/meme.txt" "${WORKDIR}/${REF}"
+fimo --bfile --motif-- -oc "${WORKDIR}/motif/${GENE}/fimo" "${WORKDIR}/motif/${GENE}/meme/meme.txt" "${WORKDIR}/${REF}"
 
 mast -bfile --motif-- -remcorr -oc "${WORKDIR}/motif/${GENE}/mast" "${WORKDIR}/motif/${GENE}/meme/meme.txt" "${WORKDIR}/${BLAST_RES}"
